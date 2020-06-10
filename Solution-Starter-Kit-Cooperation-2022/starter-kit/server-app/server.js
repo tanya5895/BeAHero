ab4e282ad565d6f0178b4d3628169a80a924d64c
tshriv13 <tshriv13@in.ibm.com>
2020-06-11 03:15:12 +0530

    added order placing functionality

--- Solution-Starter-Kit-Cooperation-2022/starter-kit/server-app/server.js
+++ Solution-Starter-Kit-Cooperation-2022/starter-kit/server-app/server.js
@@ -1,39 +1,40 @@
-require('dotenv').config({silent: true})
+require("dotenv").config({ silent: true });
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const assistant = require('./lib/assistant.js');
-const port = process.env.PORT || 3000
+const assistant = require("./lib/assistant.js");
+const port = process.env.PORT || 3000;
 
-const cloudant = require('./lib/cloudant.js');
+const cloudant = require("./lib/cloudant.js");
 
 const app = express();
 app.use(bodyParser.json());
 
 const testConnections = () => {
-  const status = {}
-  return assistant.session()
-    .then(sessionid => {
-      status['assistant'] = 'ok';
-      return status
+  const status = {};
+  return assistant
+    .session()
+    .then((sessionid) => {
+      status["assistant"] = "ok";
+      return status;
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-      status['assistant'] = 'failed';
-      return status
+      status["assistant"] = "failed";
+      return status;
     })
-    .then(status => {
+    .then((status) => {
       return cloudant.info();
     })
-    .then(info => {
-      status['cloudant'] = 'ok';
-      return status
+    .then((info) => {
+      status["cloudant"] = "ok";
+      return status;
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-      status['cloudant'] = 'failed';
-      return status
+      status["cloudant"] = "failed";
+      return status;
     });
 };
 
@@ -42,8 +43,8 @@ const handleError = (res, err) => {
   return res.status(status).json(err);
 };
 
-app.get('/', (req, res) => {
-  testConnections().then(status => res.json({ status: status }));
+app.get("/", (req, res) => {
+  testConnections().then((status) => res.json({ status: status }));
 });
 
 /**
@@ -51,11 +52,11 @@ app.get('/', (req, res) => {
  *
  * Returns a session ID that can be used in subsequent message API calls.
  */
-app.get('/api/session', (req, res) => {
+app.get("/api/session", (req, res) => {
   assistant
     .session()
-    .then(sessionid => res.send(sessionid))
-    .catch(err => handleError(res, err));
+    .then((sessionid) => res.send(sessionid))
+    .catch((err) => handleError(res, err));
 });
 
 /**
@@ -65,11 +66,11 @@ app.get('/api/session', (req, res) => {
  * look up in the Cloudant DB whether any of the requested resources are
  * available. If so, we insert a list of the resouces found into the response
  * that will sent back to the client.
- * 
+ *
  * We also modify the text response to match the above.
  */
 function post_process_assistant(result) {
-  let resource
+  let resource;
   // First we look to see if a) Watson did identify an intent (as opposed to not
   // understanding it at all), and if it did, then b) see if it matched a supplies entity
   // with reasonable confidence. "supplies" is the term our trained Watson skill uses
@@ -81,29 +82,29 @@ function post_process_assistant(result) {
   //
   // That's our trigger to do a lookup - using the entitty.value as the name of resource
   // to to a datbase lookup.
-  if (result.intents.length > 0 ) {
-    result.entities.forEach(item => {
-      if ((item.entity == "supplies") &&  (item.confidence > 0.3)) {
-        resource = item.value
+  if (result.intents.length > 0) {
+    result.entities.forEach((item) => {
+      if (item.entity == "supplies" && item.confidence > 0.3) {
+        resource = item.value;
       }
-    })
+    });
   }
   if (!resource) {
-    return Promise.resolve(result)
+    return Promise.resolve(result);
   } else {
     // OK, we have a resource...let's look this up in the DB and see if anyone has any.
-    return cloudant
-      .find('', resource, '')
-      .then(data => {
-        let processed_result = result
-        if ((data.statusCode == 200) && (data.data != "[]")) {
-          processed_result["resources"] = JSON.parse(data.data)
-          processed_result["generic"][0]["text"] = 'There is' + '\xa0' + resource + " available"
-        } else {
-          processed_result["generic"][0]["text"] = "Sorry, no" + '\xa0' + resource + " available"           
-        }
-        return processed_result
-      })
+    return cloudant.find("", resource, "").then((data) => {
+      let processed_result = result;
+      if (data.statusCode == 200 && data.data != "[]") {
+        processed_result["resources"] = JSON.parse(data.data);
+        processed_result["generic"][0]["text"] =
+          "There is" + "\xa0" + resource + " available";
+      } else {
+        processed_result["generic"][0]["text"] =
+          "Sorry, no" + "\xa0" + resource + " available";
+      }
+      return processed_result;
+    });
   }
 }
 
@@ -111,145 +112,230 @@ function post_process_assistant(result) {
  * Post a messge to Watson Assistant
  *
  * The body must contain:
- * 
+ *
  * - Message text
  * - sessionID (previsoulsy obtained by called /api/session)
  */
-app.post('/api/message', (req, res) => {
-  const text = req.body.text || '';
+app.post("/api/message", (req, res) => {
+  const text = req.body.text || "";
   const sessionid = req.body.sessionid;
-  console.log(req.body)
+  console.log(req.body);
   assistant
     .message(text, sessionid)
-    .then(result => {
-      return post_process_assistant(result)
+    .then((result) => {
+      return post_process_assistant(result);
     })
-    .then(new_result => {
-      res.json(new_result)
+    .then((new_result) => {
+      res.json(new_result);
     })
-    .catch(err => handleError(res, err));
+    .catch((err) => handleError(res, err));
 });
 
 /**
  * Get a list of resources
  *
  * The query string may contain the following qualifiers:
- * 
+ *
  * - type
  * - name
  * - userID
  *
  * A list of resource objects will be returned (which can be an empty list)
  */
-app.get('/api/resource', (req, res) => {
+app.get("/api/resource", (req, res) => {
   const type = req.query.type;
   const name = req.query.name;
   const userID = req.query.userID;
+  const orderPlaced = req.query.orderPlaced;
   cloudant
-    .find(type, name, userID)
-    .then(data => {
+    .find(type, name, userID, orderPlaced)
+    .then((data) => {
       if (data.statusCode != 200) {
-        res.sendStatus(data.statusCode)
+        res.sendStatus(data.statusCode);
       } else {
-        res.send(data.data)
+        console.log("RESPONSE FOR GET IS ", data.data);
+        res.send(data.data);
       }
     })
-    .catch(err => handleError(res, err));
+    .catch((err) => handleError(res, err));
 });
 
 /**
  * Create a new resource
  *
  * The body must contain:
- * 
+ *
  * - type
  * - name
  * - contact
  * - userID
  *
  * The body may also contain:
- * 
+ *
  * - description
  * - quantity (which will default to 1 if not included)
- * 
+ *
  * The ID and rev of the resource will be returned if successful
  */
-let types = ["Food", "Other", "Help"]
-app.post('/api/resource', (req, res) => {
+let types = ["Food", "Other", "Help"];
+app.post("/api/resource", (req, res) => {
+  console.log("POST BODY ---------- ", req.body);
   if (!req.body.type) {
-    return res.status(422).json({ errors: "Type of item must be provided"});
+    return res.status(422).json({ errors: "Type of item must be provided" });
   }
   if (!types.includes(req.body.type)) {
-    return res.status(422).json({ errors: "Type of item must be one of " + types.toString()});
+    return res
+      .status(422)
+      .json({ errors: "Type of item must be one of " + types.toString() });
   }
   if (!req.body.name) {
-    return res.status(422).json({ errors: "Name of item must be provided"});
+    return res.status(422).json({ errors: "Name of item must be provided" });
   }
   if (!req.body.contact) {
-    return res.status(422).json({ errors: "A method of conact must be provided"});
+    return res
+      .status(422)
+      .json({ errors: "A method of conact must be provided" });
   }
   const type = req.body.type;
   const name = req.body.name;
-  const description = req.body.description || '';
-  const userID = req.body.userID || '';
+  const description = req.body.description || "";
+  const userID = req.body.userID || "";
   const quantity = req.body.quantity || 1;
-  const location = req.body.location || '';
+  const location = req.body.location || "";
   const contact = req.body.contact;
+  const NGO = req.body.NGO;
+  const orderPlaced = req.body.orderPlaced;
 
   cloudant
-    .create(type, name, description, quantity, location, contact, userID)
-    .then(data => {
+    .create(
+      type,
+      name,
+      description,
+      quantity,
+      location,
+      contact,
+      userID,
+      NGO,
+      orderPlaced
+    )
+    .then((data) => {
       if (data.statusCode != 201) {
-        res.sendStatus(data.statusCode)
+        res.sendStatus(data.statusCode);
       } else {
-        res.send(data.data)
+        console.log("POST DATA IS ", data.data);
+        res.send(data.data);
       }
     })
-    .catch(err => handleError(res, err));
+    .catch((err) => handleError(res, err));
 });
+
+// let types = ["Food", "Other", "Help"];
+// app.post("/api/placeOrder", (req, res) => {
+//   console.log("POST BODY ---------- ", req.body);
+//   if (!req.body.type) {
+//     return res.status(422).json({ errors: "Type of item must be provided" });
+//   }
+//   if (!types.includes(req.body.type)) {
+//     return res
+//       .status(422)
+//       .json({ errors: "Type of item must be one of " + types.toString() });
+//   }
+//   if (!req.body.name) {
+//     return res.status(422).json({ errors: "Name of item must be provided" });
+//   }
+//   if (!req.body.contact) {
+//     return res
+//       .status(422)
+//       .json({ errors: "A method of conact must be provided" });
+//   }
+//   const type = req.body.type;
+//   const name = req.body.name;
+//   const description = req.body.description || "";
+//   const userID = req.body.userID || "";
+//   const quantity = req.body.quantity || 1;
+//   const location = req.body.location || "";
+//   const contact = req.body.contact;
+//   const NGO = req.body.NGO;
+//   const orderPlaced = req.body.orderPlaced;
+
+//   cloudant
+//     .createOrder(
+//       type,
+//       name,
+//       description,
+//       quantity,
+//       location,
+//       contact,
+//       userID,
+//       NGO,
+//       orderPlaced
+//     )
+//     .then((data) => {
+//       if (data.statusCode != 201) {
+//         res.sendStatus(data.statusCode);
+//       } else {
+//         console.log("POST DATA IS ", data.data);
+//         res.send(data.data);
+//       }
+//     })
+//     .catch((err) => handleError(res, err));
+// });
 
 /**
  * Update new resource
  *
  * The body may contain any of the valid attributes, with their new values. Attributes
  * not included will be left unmodified.
- * 
+ *
  * The new rev of the resource will be returned if successful
  */
 
-app.patch('/api/resource/:id', (req, res) => {
-  const type = req.body.type || '';
-  const name = req.body.name || '';
-  const description = req.body.description || '';
-  const userID = req.body.userID || '';
-  const quantity = req.body.quantity || '';
-  const location = req.body.location || '';
-  const contact = req.body.contact || '';
+app.patch("/api/resource/:id", (req, res) => {
+  const type = req.body.type || "";
+  const name = req.body.name || "";
+  const description = req.body.description || "";
+  const userID = req.body.userID || "";
+  const quantity = req.body.quantity || "";
+  const location = req.body.location || "";
+  const contact = req.body.contact || "";
+  const NGO = req.body.NGO || "";
 
   cloudant
-    .update(req.params.id, type, name, description, quantity, location, contact, userID)
-    .then(data => {
+    .update(
+      req.params.id,
+      type,
+      name,
+      description,
+      quantity,
+      location,
+      contact,
+      userID,
+      NGO
+    )
+    .then((data) => {
       if (data.statusCode != 200) {
-        res.sendStatus(data.statusCode)
+        res.sendStatus(data.statusCode);
       } else {
-        res.send(data.data)
+        res.send(data.data);
       }
     })
-    .catch(err => handleError(res, err));
+    .catch((err) => handleError(res, err));
 });
 
 /**
  * Delete a resource
  */
-app.delete('/api/resource/:id', (req, res) => {
+app.delete("/api/resource/:id", (req, res) => {
   cloudant
     .deleteById(req.params.id)
-    .then(statusCode => res.sendStatus(statusCode))
-    .catch(err => handleError(res, err));
+    .then((statusCode) => res.sendStatus(statusCode))
+    .catch((err) => handleError(res, err));
 });
 
 const server = app.listen(port, () => {
-   const host = server.address().address;
-   const port = server.address().port;
-   console.log(`SolutionStarterKitCooperationServer listening at http://${host}:${port}`);
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log(
+    `SolutionStarterKitCooperationServer listening at http://${host}:${port}`
+  );
 });
